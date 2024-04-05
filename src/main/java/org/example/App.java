444dfbc2b5ef4d91140bbd7bf218711878b3c490
psm817 <5004jse@gmail.com>
2024-04-05 17:51:31 +0900

    서비스 다시 보기 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,21 +6,7 @@ import java.util.Scanner;
 
 public class App {
     public void start() {
-        System.out.println("호텔 예약 관리 시스템에 오신 것을 환영합니다.");
-        System.out.printf("오늘 날짜 : %s", Util.getTodayDate());
-        System.out.printf("\n");
-        System.out.println("=============== 서비스 모음 ===============");
-        System.out.println("1. 호텔 소개 : hotel introduce");
-        System.out.println("2. 객실 예약현황 : room list");
-        System.out.println("3. 객실 예약하기 : booking room");        // id, roomId, floor, guestName, guestPhone, type, regDate
-        System.out.println("4. 예약 확인하기 : booking check");
-        System.out.println("5. 예약정보 수정 : booking modify");
-        System.out.println("6. 예약 취소 : booking delete");
-        System.out.println("7. 로그인/로그아웃 : guest login/logout");
-        System.out.println("8. 회원 가입 : guest join");
-        System.out.println("9. 리뷰 남기기 : booking review");
-        System.out.println("===========================================");
-        System.out.printf("\n");
+        callAllServices();
 
         HotelController hotelController = new HotelController();
         RoomController roomController = new RoomController();
@@ -44,6 +30,11 @@ public class App {
 
             if(cmd.equals("exit")) {
                 break;
+            }
+
+            if(cmd.equals("service")) {
+                callAllServices();
+                continue;
             }
 
             String[] cmdBits = cmd.split(" ");
@@ -109,5 +100,24 @@ public class App {
 
         sc.close();
         System.out.println("감사합니다. 다음에 또 방문해주세요.");
+    }
+
+    private void callAllServices() {
+        System.out.println("호텔 예약 관리 시스템에 오신 것을 환영합니다.");
+        System.out.printf("오늘 날짜 : %s", Util.getTodayDate());
+        System.out.printf("\n");
+        System.out.println("=============== 서비스 모음 ===============");
+        System.out.println("1. 호텔 소개 : hotel introduce");
+        System.out.println("2. 객실 예약현황 : room list");
+        System.out.println("3. 객실 예약하기 : booking room");        // id, roomId, floor, guestName, guestPhone, type, regDate
+        System.out.println("4. 예약 확인하기 : booking check");
+        System.out.println("5. 예약정보 수정 : booking modify");
+        System.out.println("6. 예약 취소 : booking delete");
+        System.out.println("7. 로그인/로그아웃 : guest login/logout");
+        System.out.println("8. 회원 가입 : guest join");
+        System.out.println("9. 리뷰 남기기 : booking review");
+        System.out.println("10. 서비스 다시보기 : service");
+        System.out.println("===========================================");
+        System.out.printf("\n");
     }
 }
