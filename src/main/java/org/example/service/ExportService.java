602d5e03f6b527bd5c7232e74363f42ccdbe158f
psm817 <5004jse@gmail.com>
2024-04-16 16:48:34 +0900

    export html 정말 간단한 css 및 버튼 추가

--- src/main/java/org/example/service/ExportService.java
+++ src/main/java/org/example/service/ExportService.java
@@ -29,17 +29,31 @@ public class ExportService {
 
             String fileName = review.id + "번 리뷰.html";
             String html = "<meta charset=\"UTF-8\">";
-            html += "<h1>리뷰</h1>";
-            html += "<div>번호 : " + review.id + "</div>";
+            html += "<style>";
+            html += "body { font-family: Arial, sans-serif; }";
+            html += "div { margin-bottom: 10px; font-weight: bold; }";
+            html += ".btn {display: inline-block; margin-right: 10px; }";
+            html += "h1 { color: navy; }";
+            html += "a { color: inherit; text-decoration: none; }";
+            html += "a { display: inline-block; margin-top: 20px; padding: 10px 20px; border-radius: 8px; border: 1px solid black; background-color: #afafaf; }";
+            html += "a:hover { background-color: #000; color: #fff; }";
+            html += "</style>";
+            html += "</head>";
+            html += "<body>";
+            html += "<h1>숙박 후기</h1>";
+            html += "<div>리뷰번호 : " + review.id + "</div>";
             html += "<div>객실번호 : " + replyBooking.roomId + "</div>";
-            html += "<div>평점 : " + review.score + "</div>";
+            html += "<div>평점 : " + review.score + " / 5" + "</div>";
             html += "<div>작성자ID : " + replyGuest.loginId + "</div>";
             html += "<div>내용 : " + review.body + "</div>";
 
             if(review.id > 1) {
-                html += "<div><a href=\"" + (review.id - 1) + "번 리뷰.html\">이전글</a></div>";
+                html += "<div class=\"btn\"><a href=\"" + (review.id - 1) + "번 리뷰.html\">이전글</a></div>";
             }
-            html += "<div><a href=\"" + (review.id + 1) + "번 리뷰.html\">다음글</a></div>";
+
+            if(review.id < reviews.size()) {
+                html += "<div class=\"btn\"><a href=\"" + (review.id + 1) + "번 리뷰.html\">다음글</a></div>";
+            }
 
             Util.writeFileContents("exportHtml/" + fileName, html);
         }
